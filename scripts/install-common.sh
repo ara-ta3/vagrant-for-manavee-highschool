0c8a2918c7a7fc09551b3d3f3f24de402a0cfaf2
Arata Tanaka <tarata43@yahoo.co.jp>
2014-11-03 14:14:37 +0900

    昔書いた設定を反映

--- scripts/install-common.sh
+++ scripts/install-common.sh
@@ -1,6 +1,15 @@
 #remi
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+
 yum update
-yum install -y php --enablerepo=remi
-yum install -y mysql-devel mysql-server
+yum install -y openssl openssl-devel zsh vim git
+yum install -y rpm-build redhat-rpm-config gcc gcc-c++ make 
+
+# PHP
+yum install -y php54-{cli,pdo,mysql,common php54,cgi,fpm,devel,gd,mbstring,odbc,pear,snmp,bcmath,xmlrpc,tidy,xml,mssql,soap,pgsql}
+
+# mysql
+yum install -y MySQL-{client,devel,server,shared-compat}-5.6.21-1.el6.x86_64.rpm
+
+
 
